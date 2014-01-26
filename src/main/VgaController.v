940c124b1708102c50f292b71d95b6bef65662b8
Josh DeWitt <chessami92@gmail.com>
2014-01-26 09:48:05 -0600

    Got the entire state machine working properly and simulated.

--- src/main/VgaController.v
+++ src/main/VgaController.v
@@ -8,11 +8,11 @@ module VgaController(
 	);
 
 	parameter vFrontPorch = 3'b000, vPulse = 3'b001, vBackPorch = 3'b010,
-	          hFrontPorch = 3'b011, hPulse = 3'b100, hBackPorch = 3'b101,
-				 display = 3'b110;
+	          display = 3'b011,
+	          hFrontPorch = 3'b100, hPulse = 3'b101, hBackPorch = 3'b110;
 
 	reg [9:0] hCounter;
-	reg [9:0] vCounter;
+	reg [8:0] vCounter;
 	reg [2:0] state;
 	reg clkDiv;
 
@@ -20,7 +20,7 @@ module VgaController(
 		if( rst == 1'b0 ) begin
 			state <= vFrontPorch;
 			hCounter <= 10'b0;
-			vCounter <= 10'b0;
+			vCounter <= 9'b0;
 			vgaRed <= 1'b1;
 			vgaGreen <= 1'b0;
 			vgaBlue <= 1'b0;
@@ -29,11 +29,21 @@ module VgaController(
 			hCounter <= hCounter + 10'b1;
 			if( hCounter == 799 ) begin
 				hCounter <= 10'b0;
-				vCounter <= vCounter + 10'b1;
+				vCounter <= vCounter + 9'b1;
 				if( ( state == vFrontPorch && vCounter == 9 ) || ( state == vPulse && vCounter == 1 ) || ( state == vBackPorch && vCounter == 28 ) ) begin
 					state <= state + 1;
-					vCounter <= 10'b0;
+					vCounter <= 9'b0;
 				end
+				else if( state == hBackPorch && vCounter != 479 ) begin
+					state <= display;
+				end
+				else if( state == hBackPorch && vCounter == 479 ) begin
+					state <= vFrontPorch;
+					vCounter <= 9'b0;
+				end
+			end
+			else if( ( state == display && hCounter == 639 ) || ( state == hFrontPorch && hCounter == 655 ) || ( state == hPulse && hCounter == 751 ) ) begin
+				state <= state + 1;
 			end
 		end
 	end
--- src/test/VgaControllerTest.v
+++ src/test/VgaControllerTest.v
@@ -9,6 +9,7 @@ module VgaControllerTest;
 	wire vSync, hSync;
 	
 	reg vSyncChange, hSyncChange;
+	integer i;
 
 	VgaController uut (
 		.clk( clk ), 
@@ -25,16 +26,34 @@ module VgaControllerTest;
 		rst = 0;
 		vSyncChange = 1'b1;
 		hSyncChange = 1'b1;
-		
+
 		#25 `assert( hSync == 1'b1 && vSync == 1'b1 );
 		rst = 1;
 		vSyncChange = 1'b0;
 		hSyncChange = 1'b0;
-		
+
+		/* Initial vSync Pulse */
 		#319974 vSyncChange = 1'b1;
 		#2 vSyncChange = 1'b0;
 		#63998 vSyncChange = 1'b1;
 		#2 vSyncChange = 1'b0;
+		#928000; /* vSync back porch */
+
+		while( 1 ) begin
+			for( i = 0; i < 480; i = i + 1 ) begin
+				#25600 #638 hSyncChange = 1'b1;
+				#2 hSyncChange = 1'b0;
+				#3838 hSyncChange = 1'b1;
+				#2 hSyncChange = 1'b0;
+				#1920; /* hSync back porch */
+			end
+
+			#319998 vSyncChange = 1'b1;
+			#2 vSyncChange = 1'b0;
+			#63998 vSyncChange = 1'b1;
+			#2 vSyncChange = 1'b0;
+			#928000; /* vSync back porch */
+		end
 	end
 	
 	always @( vSync ) begin
