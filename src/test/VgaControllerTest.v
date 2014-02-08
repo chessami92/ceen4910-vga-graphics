27075da9e7836efb5f9591f5f63226b24e126f26
Josh DeWitt <chessami92@gmail.com>
2014-02-08 14:22:48 -0600

    Stopped overspecifying the desired values.

--- src/test/VgaControllerTest.v
+++ src/test/VgaControllerTest.v
@@ -22,44 +22,44 @@ module VgaControllerTest;
 	initial begin
 		clk = 1;
 		rst = 1;
-		vSyncChange = 1'b1;
-		hSyncChange = 1'b1;
+		vSyncChange = 1;
+		hSyncChange = 1;
 
-		#25 `assert( hSync == 1'b0 && vSync == 1'b1 );
+		#25 `assert( hSync == 0 && vSync == 1 );
 		rst = 0;
-		vSyncChange = 1'b0;
-		hSyncChange = 1'b0;
+		vSyncChange = 0;
+		hSyncChange = 0;
 		
 		/* Initial hSyncPulse */
-		#3814 hSyncChange = 1'b1; /* hSyncWidth */
-		#2 hSyncChange = 1'b0;
+		#3814 hSyncChange = 1; /* hSyncWidth */
+		#2 hSyncChange = 0;
 
 		while( 1 ) begin
 			for( i = 0; i < 479; i = i + 1 ) begin
-				#28158 hSyncChange = 1'b1; /* hFrontPorch, hDisplay, and hBackPorch */
-				#2 hSyncChange = 1'b0;
-				#3838 hSyncChange = 1'b1; /* hSyncWidth */
-				#2 hSyncChange = 1'b0;
+				#28158 hSyncChange = 1; /* hFrontPorch, hDisplay, and hBackPorch */
+				#2 hSyncChange = 0;
+				#3838 hSyncChange = 1; /* hSyncWidth */
+				#2 hSyncChange = 0;
 			end
 			#28160 /* hBackPorch, hDisplay, and hFrontPorch */
 
-			#1055998; vSyncChange = 1'b1; /* vBackPorch */
-			#2 vSyncChange = 1'b0;
-			#63998 vSyncChange = 1'b1; /* vSyncWidth */
-			#2 vSyncChange = 1'b0;
-			#319998 hSyncChange = 1'b1; /* vFrontPorch */
-			#2 hSyncChange = 1'b0;
-			#3838 hSyncChange = 1'b1; /* hSyncWidth */
-			#2 hSyncChange = 1'b0;
+			#1055998; vSyncChange = 1; /* vBackPorch */
+			#2 vSyncChange = 0;
+			#63998 vSyncChange = 1; /* vSyncWidth */
+			#2 vSyncChange = 0;
+			#319998 hSyncChange = 1; /* vFrontPorch */
+			#2 hSyncChange = 0;
+			#3838 hSyncChange = 1; /* hSyncWidth */
+			#2 hSyncChange = 0;
 		end
 	end
 	
 	always @( vSync ) begin
-		`assert( vSyncChange == 1'b1 );
+		`assert( vSyncChange );
 	end
 	
 	always @( hSync ) begin
-		`assert( hSyncChange == 1'b1 );
+		`assert( hSyncChange );
 	end
 	
 	always begin
