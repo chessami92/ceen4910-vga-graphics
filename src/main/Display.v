c1023212949d3652d9a3d17b2da7cd7d307a76f3
Josh DeWitt <chessami92@gmail.com>
2014-02-12 22:13:30 -0600

    Added in the rotary button to allow "zooming" in and out of the columns.

--- src/main/Display.v
+++ src/main/Display.v
@@ -1,14 +1,15 @@
 `timescale 1ns / 1ps
 
 module Display(
-	input clk, rst, noise,
+	input clk, rst, noise, rotA, rotB, rotCenter,
 	output wire [2:0] color,
-	output vSync, hSync
+	output wire vSync, hSync
 	);
 
 	wire [8:0] row;
 	wire [9:0] column;
 	wire displayActive;
+	wire left, right, down;
 
 	reg clkDiv;
 
@@ -25,12 +26,27 @@ module Display(
 
 	GameOfLife colorGenerator(
 		.clk( clk ),
+		.clkDiv( clkDiv ),
 		.rst( rst ),
 		.displayActive( displayActive ),
 		.noise( noise ),
+		.increment( right ),
+		.decrement( left ),
+		.drawAgain( down ),
 		.row( row ),
 		.column( column ),
 		.color( color )
+	);
+	
+	RotaryButton rotaryButton (
+		.clk( clk ), 
+		.rst( rst ),
+		.rotA( rotA ),
+		.rotB( rotB ),
+		.rotCenter( rotCenter ), 
+		.left( left ),
+		.right( right ),
+		.down( down )
 	);
 
 	always @( posedge clk or posedge rst ) begin
